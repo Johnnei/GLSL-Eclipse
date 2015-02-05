ac8c94e236d0034df4300c1e732f9a3257fcab0d
Johnnei <johnnei07@live.nl>
2015-02-05 15:29:36 +0100

    Added preference page to select appearances

---
+++ src/org/johnnei/glsl/preferences/PreferenceConstants.java
@@ -0,0 +1,10 @@
+package org.johnnei.glsl.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_CHOICE = "choicePreference";
+	
+}
